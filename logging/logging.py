644c3bc4e97c600d96df993126c997a8cdd9ef82
jonas <jonas@5rose.ch>
2020-09-28 17:53:40 +0200

    wrote some stuff uploaded pictures for the doc and developed the logging algorithm for cron

--- logging/logging.py
+++
@@ -1,8 +0,0 @@
-# a script for keeping track of how much time i spend on which aspect of my matura work
-# data.txt 
-#research
-#coding
-#writing
-#assembly
-
-open(
--- sensors/both.py
+++ sensors/both.py
@@ -1,16 +1,24 @@
 #returns both 1W and I2C sensor values - only one I2C device
 
 
-import glob
-import os
-import adafruit_dht
+import glob, os, adafruit_dht, datetime
 from time import sleep
 ## ONEWIRE SETUP
 os.system('modprobe w1-gpio')                              # load one wire communication device kern$
 os.system('modprobe w1-therm')
 base_dir = '/sys/bus/w1/devices/'                          # point to the address
 device_folder = glob.glob(base_dir + '28*')[0]             # find device with address starting from $
-device_file = device_folder + '/w1_slave'                  # store the details
+device_file = device_folder + '/w1_slave' 
+logfile_air_temp = "log_air_temp.txt"
+logfile_air_hum = "log_air_hum.txt"
+logfile_water_temp = "log_water_temp.txt"
+
+def reset_file(file):
+   f = open(file, "w")
+   f.write("")
+   f.close
+
+# store the details
 def read_temp_raw():
    f = open(device_file, 'r')
    lines = f.readlines()                                   # read the device details
@@ -20,7 +28,7 @@ def read_temp_raw():
 def read_temp():
    lines = read_temp_raw()
    while lines[0].strip()[-3:] != 'YES':                   # ignore first line
-      time.sleep(0.2)
+      sleep(0.2)
       lines = read_temp_raw()
    equals_pos = lines[1].find('t=')                        # find temperature in the details
    if equals_pos != -1:
@@ -28,13 +36,20 @@ def read_temp():
       temp_c = float(temp_string) / 1000.0                 # convert to Celsius
       return temp_c
 
-
-
-## I2C Setup
+## I2C Setup = temp and hum sensor
 dht_device = adafruit_dht.DHT22(2)
 
+reset_file(logfile_air_temp)
+reset_file(logfile_air_hum)
+reset_file(logfile_water_temp)
+
 while True:
-        print("I2C-Temp: " + str(dht_device.temperature))
-        print("I2C-Hum: " + str(dht_device.humidity))
-        print("1W-Temp: " + str(read_temp()))
-        sleep(2)
+   logat = open(logfile_air_temp, "a")
+   logah = open(logfile_air_hum, "a")
+   logwt = open(logfile_water_temp, "a")
+      for i in range(6)
+         now = str(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+         logat.write(now+":"+str(dht_device.temperature))
+         logah.write(now+":"+str(dht_device.humidity))
+         logwt.write(now+":"+str(read_remp))
+        sleep(5)
