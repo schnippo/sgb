bea398155730c956513570cb2d05ce4d9f88be9a
schnippo <jonas@5rose.ch>
2020-10-15 23:58:30 +0200

    serial test

--- daemon/serial_test.py
+++ daemon/serial_test.py
@@ -1,24 +1,27 @@
 import serial
 from time import sleep
 
-devices = ["/dev/ttyACM0", "/dev/ttyACM1"]
+addresses = ["/dev/ttyACM0", "/dev/ttyACM1"]
 
-def try_devices():
-	for device in devices:
-		with serial.Serial(device) as ser:
-			try:
-				ser.write("9".encode())
-				sleep(2)
-				answer = ser.readline().decode()
-				if answer == "pwm_controller" or answer == "relay_controller":
-					print(device, ":", answer)
-				ser.write("99".encode())
-				sleep(2)
-				answer = ser.readline().decode()
-				if answer == "pwm_controller" or answer == "relay_controller":
-					print(device, ":", answer)
-			except:
-				print("error at", device)
+devices = [serial.Serial(addresses[0]), serial.Serial(addresses[1])]
+
+
+for device in devices:
+	try:
+		device.write("9".encode())
+		sleep(2)
+		answer = device.readline().decode()
+		if answer == "pwm_controller" or answer == "relay_controller":
+			print(device, ":", answer)
+			continue
+		else:
+			device.write("99".encode())
+			sleep(2)
+			answer = device.readline().decode()
+			if answer == "pwm_controller" or answer == "relay_controller":
+				print(device, ":", answer)
+	except:
+		print("error at", device)
 			
 
 try_devices()
