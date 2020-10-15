6e381f3ceebcc8ac3d44e6232db09deff5e1577f
schnippo <jonas@5rose.ch>
2020-10-15 19:24:25 +0200

    serialtest

---
+++ daemon/serial_test.py
@@ -0,0 +1,13 @@
+import serial
+from time import sleep
+
+devices = ["/dev/ttyACM0", "/dev/ttyACM1", "/dev/ttyACM2"]
+
+for device in devices:
+	with serial.Serial(device) as ser:
+		try:
+			ser.write("9".encode())
+			sleep(2)
+			print(ser.readline().decode())
+		except:
+			print("error at", device)
